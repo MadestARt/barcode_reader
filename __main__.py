ac4b613895bb803f09fbed88a87678e3a43517d8
asvladimirov <336448@niuitmo.ru>
2025-06-19 19:47:36 +0300

    implement stable version

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 from barcode_reader.file_checker import FileChecker
 from barcode_reader.barcode_extractor import BarcodeExtractor
+from barcode_reader.barcode_decoder import BarcodeDecoder
+
 
 def main():
     file_path = input("Введите абсолютный путь до файла штрихкода: ").strip()
@@ -15,10 +17,11 @@ def main():
         return
 
     barcode_extractor = BarcodeExtractor(file_path)
+    data_binary = barcode_extractor.get_barcode_data_bits()
 
-    data = barcode_extractor.get_barcode_data_bits()
+    barcode_decoder = BarcodeDecoder(data_binary)
+    data = barcode_decoder.decode()
 
     print(data)
-
 if __name__ == "__main__":
     main()
